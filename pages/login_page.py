18bf949bd24b3c5d02177760ba72062cf43259d3
sokolip <sokolip8@gmail.com>
2025-07-09 13:11:56 -0500

    login_page_ref

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,33 +2,39 @@ from pages.base_page import BasePage
 from playwright.sync_api import Page, expect
 import os
 from dotenv import load_dotenv
+from time import sleep
 
 load_dotenv()
 
 BASE_URL = os.getenv("BASE_URL")
 TEST_EMAIL = os.getenv("TEST_EMAIL")
+TEST_USER = os.getenv("TEST_LOGIN")
 TEST_PASSWORD = os.getenv("TEST_PASSWORD")
 
 
 class LoginPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
-        self.email_input = self.page.get_by_placeholder("Введите логин")
+        self.login_input = self.page.get_by_placeholder("Введите логин")
         self.password_input = self.page.get_by_placeholder("Пароль")
         self.login_button = self.page.get_by_role("button", name="Далее")
 
     def open_login_page(self):
         self.open_page(BASE_URL)
+        self.wait_for_page_ready("load")
         self.click_button("Вход / Регистрация")
         self.page.wait_for_url("**/login/callback/")
+        self.wait_for_page_ready("load")
         expect(self.page.get_by_text("Вход")).to_be_visible()
 
-    def login_with_email(self):
-        self.email_input.fill(TEST_EMAIL)
+    def login_with_username(self):
+        self.login_input.fill(TEST_USER)
         self.password_input.fill(TEST_PASSWORD)
         self.login_button.click()
+        sleep(30)
 
     def check_ui_elements(self):
+        self.wait_for_page_ready("networkidle")
         assert self.is_visible("//button[@title='google']"), "Кнопка авторизации через Google не отображается"
         self.expect_clickable(self.page.get_by_role("link", name="Регистрация"))
         self.expect_clickable(self.page.get_by_role("link", name="Пользовательского соглашения"))
@@ -42,7 +48,12 @@ class LoginPage(BasePage):
             self.click_button(name="Закрыть")
 
     def main_page_is_opened(self):
-        expect(self.page.get_by_text(TEST_EMAIL)).to_be_visible()
+        try:
+            expect(self.page.get_by_text("ivn.sok@yandex.ru")).to_be_visible()
+            print("Успешный логин - тестовый пользователь попал в личный кабинет")
+        except Exception as e:
+            print(f"Ошибка при открытии личного кабинета: {e}")
+            raise
 
 
 
--- tests/test_login.py
+++ tests/test_login.py
@@ -5,12 +5,12 @@ def test_login_page(page):
     login = LoginPage(page)
     login.open_login_page()
     login.check_ui_elements()
-    login.login_with_email()
+    login.login_with_username()
     login.skip_2fa()
     login.skip_greeting_message()
     login.main_page_is_opened()
 
-    print("Успешный логин - тестовый пользователь попал в личный кабинет")
+
 
 
 
